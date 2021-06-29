b84367260c97b4d1fb941aa052a4626ecf1b0c93
phunyaveepur <p.purahong@hotmail.com>
2021-06-30 00:15:55 +0700

    Edited and completed remaining steps

--- run_analysis.R
+++ run_analysis.R
@@ -12,30 +12,33 @@ src_train_Y <-  "UCI HAR Dataset/train/Y_train.txt"
 
 #Read files into Data Frame with column names defined
 feature <- read.table(src_feature, col.names = c("id","features"))
-activity <- read.table(src_activity, col.names = c("activity","activity_desc"))
+activity <- read.table(src_activity, col.names = c("activityID","activity"))
 test_subject <- read.table(src_test_subject, col.names = c("subject"))
 test_x <- read.table(src_test_X, col.names = feature$features)
-test_y <- read.table(src_test_Y, col.names = c("activity"))
+test_y <- read.table(src_test_Y, col.names = c("activityID"))
 train_subject <- read.table(src_train_subject, col.names = c("subject"))
 train_x <- read.table(src_train_X, col.names = feature$features)
-train_y <- read.table(src_train_Y, col.names = c("activity"))
+train_y <- read.table(src_train_Y, col.names = c("activityID"))
+
+#Select column ID for columns which contain MEAN or STD
+sel_features <- feature[grepl("[Mm]ean|[Ss]td",feature$features),1] 
+
+#Merge activity description column
+activity_test <- merge(test_y, activity, by.x = "activityID", by.y = "activityID")
+activity_train <- merge(train_y, activity, by.x = "activityID", by.y = "activityID")
 
 #Merge all for each set and then combine them altogether
-test_data <- cbind(test_subject, test_x, test_y)
-train_data <- cbind(train_subject, train_x, train_y)
-full_data <- rbind(test_data, train_data)
+#Select only columns which present MEAN or STD from full_data using sel_features
+test_data <- cbind(test_subject, activity_test, test_x[, sel_features])
+train_data <- cbind(train_subject, activity_train,train_x[, sel_features])
+tidy_data <- rbind(test_data, train_data)
+tidy_data <- select(tidy_data, !activityID)
 
 #Check if full_data is complete
 dim(test_data)
 dim(train_data)
-dim(full_data)
-nrow(full_data) == nrow(test_data)+nrow(train_data) #should provide result as TRUE
-
-#Select column ID for columns which contain MEAN or STD
-sel_features <- feature[grepl("[Mm]ean|[Ss]td",feature$features),1] +1
-
-#Select only columns which present MEAN or STD from full_data using sel_features
-tidy_data <- full_data[, sel_features]
+dim(tidy_data)
+nrow(tidy_data) == nrow(test_data)+nrow(train_data) #should provide result as TRUE
 
 #Revise column names 
 names(tidy_data) <- names(tidy_data) %>% 
@@ -52,4 +55,8 @@ names(tidy_data) <- names(tidy_data) %>%
         lapply(function(x) {x <- gsub("mean", "Mean", x)}) %>% 
         lapply(function(x) {x <- gsub("[Ss][Tt][Dd]", "STD", x)}) 
 
-
+#Average each variable for each activity and each subject
+Average_All <- tidy_data %>% 
+        group_by(subject, activity) %>% 
+        summarise_all(funs(mean))
+write.table(Average_All, "Average_All.txt", row.name=FALSE)
